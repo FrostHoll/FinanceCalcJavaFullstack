5f60b27345589787035fcc4f02160c0bba9b8201
Oleg Shcherbakov <59744113+FrostHoll@users.noreply.github.com>
2023-11-10 07:05:19 +0500

    Added pages - all users page - profile page

--- src/main/java/com/frostholl/financeCalcBackend/user/UserController.java
+++ src/main/java/com/frostholl/financeCalcBackend/user/UserController.java
@@ -1,12 +1,13 @@
 package com.frostholl.financeCalcBackend.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-@RestController
-@RequestMapping("api/v1/user")
+@Controller
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
@@ -16,13 +17,27 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping
-    public List<User> getUsers() {
-        return userService.getUsers();
+    @GetMapping("/all")
+    public String getUsers(Model model) {
+        model.addAttribute("users", userService.getUsers());
+        return "users/allUsers";
     }
 
-    @PostMapping
-    public void registerNewUser(@RequestBody User user) {
-        userService.addNewUser(user);
+    @GetMapping("/edit")
+    public String editProfile(@AuthenticationPrincipal User user, Model model) {
+        model.addAttribute("user", user);
+        return "users/profile";
+    }
+
+    @PostMapping("/edit")
+    public String saveChanges(@AuthenticationPrincipal User user,
+                              @RequestParam(name = "login") String login,
+                              @RequestParam(name = "user_surname") String surname,
+                              @RequestParam(name = "user_name") String name) {
+        user.setLogin(login);
+        user.setUser_surname(surname);
+        user.setUser_name(name);
+        userService.updateUser(user);
+        return "redirect:/home";
     }
 }
