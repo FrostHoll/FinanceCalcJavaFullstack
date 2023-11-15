b21b7e8a40c718cc0dfb1cc2c9ad9fba759a225d
Oleg Shcherbakov <59744113+FrostHoll@users.noreply.github.com>
2023-11-15 17:38:28 +0500

    CSS work - added check_calculate query at the home page

--- src/main/java/com/frostholl/financeCalcBackend/controllers/HomeController.java
+++ src/main/java/com/frostholl/financeCalcBackend/controllers/HomeController.java
@@ -3,10 +3,15 @@ package com.frostholl.financeCalcBackend.controllers;
 import com.frostholl.financeCalcBackend.category.CategoryService;
 import com.frostholl.financeCalcBackend.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.object.GenericSqlQuery;
+import org.springframework.jdbc.object.SqlFunction;
+import org.springframework.jdbc.object.SqlQuery;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.sql.DataSource;
 
 @Controller
 public class HomeController {
@@ -14,11 +19,15 @@ public class HomeController {
     @Autowired
     private CategoryService categoryService;
 
+    @Autowired
+    private DataSource dataSource;
+
     @GetMapping("/home")
     public String home(@AuthenticationPrincipal User user, Model model) {
         String name = user.getUser_surname() + " " + user.getUser_name();
         model.addAttribute("username", name);
-        model.addAttribute("categories", categoryService.getAllCategories());
+        SqlQuery<Integer> query = new SqlFunction<Integer>(dataSource,"select check_recalculate();");
+        query.execute();
         return "home";
     }
 }
